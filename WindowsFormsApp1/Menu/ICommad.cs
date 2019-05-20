45f9f8975c7c671774f18cd9967fd0fb3014a340
lc <9999>
2019-05-20 18:35:15 +0800

    1

---
+++ WindowsFormsApp1/Menu/ICommad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp1.Menu
+{
+    public interface ICommand
+    {
+        //
+        // 摘要:
+        //     当出现影响是否应执行该命令的更改时发生。
+        event EventHandler CanExecuteChanged;
+
+        //
+        // 摘要:
+        //     定义确定此命令是否可在其当前状态下执行的方法。
+        //
+        // 参数:
+        //   parameter:
+        //     此命令使用的数据。如果此命令不需要传递数据，则该对象可以设置为 null。
+        //
+        // 返回结果:
+        //     如果可执行此命令，则为 true；否则为 false。
+        bool CanExecute(object parameter);
+        //
+        // 摘要:
+        //     定义在调用此命令时要调用的方法。
+        //
+        // 参数:
+        //   parameter:
+        //     此命令使用的数据。如果此命令不需要传递数据，则该对象可以设置为 null。
+        void Execute(object parameter);
+    }
+}
---
+++ WindowsFormsApp1/Menu/TitelMenuCommand.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Autodesk.AutoCAD.Interop;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+[assembly: CommandClass(typeof(RegulatoryPlan.Menu.TitelMenuCommand))]
+namespace RegulatoryPlan.Menu
+{
+   
+    public class TitelMenuCommand 
+    {
+        [CommandMethod("CAKGM")]
+        public void ShowMyMenu()
+        {  //获取CAD应用程序
+            AcadApplication app = (AcadApplication)Autodesk.AutoCAD.ApplicationServices.Application.AcadApplication;
+            for (int i = 0; i < app.MenuGroups.Item(0).Menus.Count; i++)
+            {
+                if (app.MenuGroups.Item(0).Menus.Item(i).Name == "成安控规")  //判断菜单是否已存在，如果存在则不再创建
+                    return;
+            }
+            
+
+           AcadPopupMenu pmParnet = app.MenuGroups.Item(0).Menus.Add("成安控规");  //添加根菜单
+            
+
+            ////多级
+            //AcadPopupMenu pm = pmParnet.AddSubMenu(pmParnet.Count + 1, "打开");
+            //AcadPopupMenuItem pmi0 = pm.AddMenuItem(pm.Count + 1, "文件  ", "OPEN1\n");  //第一个参数是在菜单项中的位置（第几项），第二个参数是显示的名称，第三个参数是点击之后执行的命令
+            //AcadPopupMenuItem pmi1 = pm.AddMenuItem(pm.Count + 1, "模版  ", "OPEN2\n");
+
+            //单级
+            AcadPopupMenuItem pmi2 = pmParnet.AddMenuItem(pmParnet.Count + 1, "数据导出 ", "ShowMainForm\n");
+
+            //将创建的菜单加入到CAD的菜单中
+            pmParnet.InsertInMenuBar(app.MenuBar.Count + 1);
+        }
+    }
+}
