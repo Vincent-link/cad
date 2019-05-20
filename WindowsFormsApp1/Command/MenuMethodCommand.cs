53fe8463b160ea0af52ea94787587733400f9ca7
lc <9999>
2019-05-20 18:35:31 +0800

    1

---
+++ WindowsFormsApp1/Command/MenuMethodCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autodesk.AutoCAD.Runtime;
+using RegulatoryPlan.UI;
+[assembly: CommandClass(typeof(RegulatoryPlan.Command.MenuMethodCommand))]
+namespace RegulatoryPlan.Command
+{
+   public class MenuMethodCommand
+    {
+        [CommandMethod("ShowMainForm")]
+        public void ShowMain()
+        {
+            MainForm form = new MainForm();
+            form.ShowDialog();
+        }
+
+    }
+}
